ef0c6c813d21e73ce7f3f1dd41663594f2b852eb
Mark Adrian Remigio <remigiomark2004@gmail.com>
2025-12-02 12:24:28 +0800

    fixed bug white space if reload

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,3 @@ const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(<App />);
-
-const removeInitialLoader = () => {
-  const loader = document.getElementById("initial-loader");
-  if (!loader) return;
-  setTimeout(() => loader.remove(), 2200);
-};
-
-if (document.readyState === "complete") {
-  removeInitialLoader();
-} else {
-  window.addEventListener("load", removeInitialLoader);
-}
